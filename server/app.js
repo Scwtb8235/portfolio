bbab2e48614c7556aac0d664b81712a04665517d
Mulugeta Getahun <mulugetagetahun@Mulugetas-MacBook-Air.local>
2026-06-15 15:54:31 -0400

    added server setup

--- server/app.js
+++ server/app.js
@@ -4,3 +4,13 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+app.use(helmet());
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({ message: 'API running' });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
