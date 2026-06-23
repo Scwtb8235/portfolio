37b334bda6462e3ecf27551f11320c94d4c74405
Mulugeta Getahun <mulugetagetahun@Mulugetas-MacBook-Air.local>
2026-06-22 22:11:13 -0400

    added About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 function App(){
   return(
     <Routes>
     <Route path="/" element={<h1>Home</h1>} />
+    <Route path="/about" element ={<About/>} />
     </Routes>
   )
 };
