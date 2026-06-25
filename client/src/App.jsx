dea278dbaba03bf5da6c32371873ddb3ac3e3189
Mulugeta Getahun <mulugetagetahun@Mulugetas-MacBook-Air.local>
2026-06-24 22:13:09 -0400

    added navbar with routing links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './pages/Navbar';
 function App(){
   return(
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element ={<About/>} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element ={<About/>} />
+      </Routes>
+    </div>
   )
 };
  export default App;
+ 
