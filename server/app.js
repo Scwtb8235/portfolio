c2c0936848a261adbcd8328fe8f15a6b7cb54796
Mulugeta Getahun <mulugetagetahun@Mulugetas-MacBook-Air.local>
2026-06-29 13:58:44 -0400

    connected mongodb

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 app.use(helmet());
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
